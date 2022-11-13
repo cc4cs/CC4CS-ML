49a0a525585e0b7cf96e6ceac22246a3118a984b
fedpaolo <federicopaolonefederico@gmail.com>
2022-11-13 23:00:15 +0100

    versione 2

--- 02_Dataset_Preparation/datasetValidation.py
+++ 02_Dataset_Preparation/datasetValidation.py
@@ -1,18 +1,19 @@
 
 import pandas as pd
 import csv
-iss=['Leon3']
+iss=['ARMV4T','Atmega328p','LEON3','ARMV6-M']
 import csv
+
+with open("TotalParameterMatrixAtmega328p.csv","r") as disco:
+# Stiamo creando un oggetto del lettore csv
+    csvreader_object=csv.reader(disco)
+# La riga salterà la prima riga del file csv (riga di intestazione)
+    features=next(csvreader_object)[0].split(';')
+
 for x in iss:
     with open("./TotalParameterMatrix"+x+".csv", newline="", encoding="ISO-8859-1") as filecsv:
         lettore = pd.read_csv(filecsv,delimiter=";")
-        print(lettore)
-        for linea in lettore:
-            if linea[32]==0 or linea[34]==0 or linea[36]==0 or linea[37]==0 or linea[38]==0 or linea[39] == 0 or linea[40]==0:
-                print('ERRORE A LINEA'+linea+'dell iss'+x)
-                break
-                
-            for i in range(1,len(linea)):
-                if linea[i]==None:
-                    print('ERRORE A LINEA'+linea+'dell iss'+x)
-                    break
+        for i in range(6,len(lettore.axes[1])):
+            if lettore.iloc[:,i][1]==0:
+                print('feature:'+features[i]+'dell iss'+x+' nella function:'+str(lettore.iloc[:,3][1])+'  vale 0')
+
